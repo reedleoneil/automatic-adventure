40348fdccf48354ce628801db7b2afa1ed96da36
reedleoneil <reedleoneilpascual@yahoo.com>
2019-02-07 01:17:15 +0900

    Update traffic light sensor.

--- embedded/traffic_light_sensor/traffic_light_sensor.ino
+++ embedded/traffic_light_sensor/traffic_light_sensor.ino
@@ -1,45 +1,43 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN 10
-#define RST_PIN 9
+#define SS_PIN 53
+#define RST_PIN 49
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
 
 int pinInductiveSensor = A0;
-int pinRelay = 13;
-byte rfid = mfrc522.uid.uidByte;
+int pinRelay = 2;
+String rfid;
 
 void setup() {
   Serial.begin(9600);
   SPI.begin();      // Init SPI bus
   mfrc522.PCD_Init(); // Init MFRC522 card
-  pinMode(pinRelay, INPUT);
+  pinMode(pinRelay, INPUT_PULLUP);
 }
 
 void loop() {
   bool isTrafficLightRed = digitalRead(pinRelay);
   int sensorReading = analogRead(pinInductiveSensor);
   readRFID();
-
-  if (isTrafficLightRed && sensorReading == 0)
-    Serial.print("{\"trafficLight\": " + String(isTrafficLightRed) + ", \"lane\": " + String(isTrafficLightRed)  + ", \"rfid\": \"" + String(rfid) +"\"}");
-
+  
+  if (!isTrafficLightRed && sensorReading == 0)
+    Serial.println("{\"trafficLight\": " + String(isTrafficLightRed) + ", \"lane\": " + String(isTrafficLightRed)  + ", \"rfid\": \"" + String(rfid) +"\"}");
   delay(1000);
 }
 
 void readRFID() {
-  //Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
+  if (mfrc522.PICC_IsNewCardPresent())
+  {
+        if (mfrc522.PICC_ReadCardSerial())
+        {
+           rfid = "";
+           for (byte i = 0; i < mfrc522.uid.size; i++) {
+                  if (String(mfrc522.uid.uidByte[i] < 0x10))
+                    rfid += " ";
+                    rfid += String(mfrc522.uid.uidByte[i], HEX);
+            }
+            mfrc522.PICC_HaltA();
+        }
   }
-
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  // Dump debug info about the card. PICC_HaltA() is automatically called.
-  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-
-  rfid = mfrc522.uid.uidByte;
 }
