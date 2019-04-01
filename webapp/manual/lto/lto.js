346ab7d19591934362de74687e57629692f0d0e3
reedleoneil <reedleoneilpascual@yahoo.com>
2019-04-01 11:01:34 +0900

    Added separate app for lto and ctmo.

---
+++ webapp/manual/lto/lto.js
@@ -0,0 +1,34 @@
+var lto = [
+  {
+    rfid: "d3 4f b7 2e",
+    plate: "CDF6143",
+    makeModel: "Honda Civic Type R 2019",
+    owner: "Lailanie Melo",
+    address: "Sta. Cruz Pambilog San Luis, Pampanga",
+    contact: "09269897407"
+  },
+  {
+    rfid: "51 dc 69 2e",
+    plate: "GHY2357",
+    makeModel: "Suzuki Swift Sport 2019",
+    owner: "Joseph Ivan Anahao",
+    address: "Muzon San Jose Del Monte Bulacan",
+    contact: "09613179978"
+  },
+  {
+    rfid: "f0 d3 f2 a2",
+    plate: "BHW1452",
+    makeModel: "Chevrolet Blazer 2019",
+    owner: "Lester Bernardino",
+    address: "San Gabriel Sta. Maria Bulacan",
+    contact: "09155227308"
+  },
+  {
+    rfid: "21 ad 79 2e",
+    plate: "BCD1432",
+    makeModel: "Toyota RAV4 2019",
+    owner: "Haniel Raymundo ",
+    address: "Bustos, Bulacan",
+    contact: "09169070498"
+  }
+];
