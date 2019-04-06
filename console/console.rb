f56825bb20e48b62fb643af00dde4752910a39f2
reedleoneil <reedleoneilpascual@yahoo.com>
2019-04-06 15:50:12 +0900

    Update console app.

--- console/console.rb
+++ console/console.rb
@@ -5,6 +5,7 @@ require 'paho-mqtt'
 
 client = PahoMqtt::Client.new()
 client.connect('localhost', 1883, client.keep_alive, true, true)
+client.subscribe(['lailanie', 2])
 
 serialport = Serial.new '/dev/ttyUSB0'
 
@@ -19,17 +20,20 @@ Thread.new {
 			x = serialport.read 1
 			if x != '' then payload << x end
 		end
+		puts payload
 		begin
 			payload = JSON.parse(payload)
-			if payload['isTrafficLightRed'] == 0 && payload['sensorReading'] == 0 then
+			if payload['isTrafficLightRed'] == 0 && payload['sensorReading'] == 0 && payload['rfid'] != "" then
 				uuid = SecureRandom.uuid
 				`fswebcam -d /dev/video0 ~/public/#{uuid}.jpg`
 				payload['image'] = "http://192.168.1.100:4567/#{uuid}.jpg"
 				payload = payload.to_json
-				client.publish("lailanie", payload, false, 1)
-				client.loop_write
-				puts payload
+			else
+				payload['image'] = ""
+				payload = payload.to_json
 			end
+			client.publish("lailanie", payload, false, 1)
+			client.loop_write
 		rescue
 
 		end
